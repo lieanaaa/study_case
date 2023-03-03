05e3c50d2d3a0e3c94a2c790fde5976bec6dd31b
Liana <lianasalsabilabjm1@gmail.com>
2023-03-03 17:58:10 +0700

    pbo

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Get
 Route::get('/apiProduk', [APiProduk_C::class, 'index']);
+// Show
 Route::get('/detailProduk/{id}', [APiProduk_C::class, 'show']);
+// Create
 Route::post('/createProduk', [APiProduk_C::class, 'store']);
+// Update 
 Route::put('/updateProduk/{id}', [APiProduk_C::class, 'update']);
+// Delete
 Route::delete('/deleteProduk/{id}', [APiProduk_C::class, 'destroy']);
-
+// Data User
 Route::get('/dataUser', [APiUser_C::class, 'index']);
+// Total Data User
 Route::get('/totalUser', [APiUser_C::class, 'show']);
